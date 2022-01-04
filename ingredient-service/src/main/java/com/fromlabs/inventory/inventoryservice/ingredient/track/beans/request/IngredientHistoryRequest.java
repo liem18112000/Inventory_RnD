5a0b724de16fe77b809537bbf0817514000b7028
Đoàn Văn Thanh Liêm <liem18112000@gmail.com>
2022-01-04 08:39:55 +0700

    Update ingredient history request

--- ingredient-service/src/main/java/com/fromlabs/inventory/inventoryservice/ingredient/track/beans/request/IngredientHistoryRequest.java
+++ ingredient-service/src/main/java/com/fromlabs/inventory/inventoryservice/ingredient/track/beans/request/IngredientHistoryRequest.java
@@ -4,9 +4,6 @@
 
 package com.fromlabs.inventory.inventoryservice.ingredient.track.beans.request;
 
-import com.fromlabs.inventory.inventoryservice.ingredient.event.IngredientEvent;
-import com.fromlabs.inventory.inventoryservice.ingredient.event.status.IngredientEventStatus;
-import com.fromlabs.inventory.inventoryservice.ingredient.track.IngredientHistoryEntity;
 import lombok.Data;
 
 @Data
@@ -15,10 +12,10 @@ public class IngredientHistoryRequest{
     private final Long      clientId;
     private final Long      ingredientId;
     private final String    name;
-    private final String    actorName = IngredientHistoryEntity.DEFAULT_ACTOR_ROLE;
-    private final String    actorRole = IngredientHistoryEntity.DEFAULT_ACTOR_ROLE;
-    private final String    event = IngredientEvent.INGREDIENT_ITEM_DEFAULT.getEvent();
-    private final String    eventStatus = IngredientEventStatus.UNKNOWN.getName();
+    private final String    actorName;
+    private final String    actorRole;
+    private final String    event;
+    private final String    eventStatus;
     private final String    description;
     private final String    extraInformation;
     private final boolean   active = true;
