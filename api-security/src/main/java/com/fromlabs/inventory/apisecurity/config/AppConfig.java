60be3e6bef139191ad2c1943fc8dfd3182350797
Đoàn Văn Thanh Liêm <liem18112000@gmail.com>
2022-01-08 15:44:08 +0700

    Add config

---
+++ api-security/src/main/java/com/fromlabs/inventory/apisecurity/config/AppConfig.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021. Liem Doan
+ */
+
+package com.fromlabs.inventory.apisecurity.config;
+
+/**
+ * Global App config variables
+ */
+public class AppConfig {
+    public static final String X_API_KEY_HEADER = "x-api-key";
+    public static final String X_PRINCIPAL_HEADER = "x-principal";
+    public static final String ADMIN_AUTHORITY = "admin";
+}
