03c279172eee068645c54197001bc69e0a572426
Đoàn Văn Thanh Liêm <liem18112000@gmail.com>
2022-01-11 08:05:36 +0700

    Fix gateway error on using other modules

--- inventory-frontend/src/constant.js
+++ inventory-frontend/src/constant.js
@@ -2,14 +2,20 @@
 /*
     Export global constant
 */
+export const VERSION            = process.env.REACT_APP_VERSION.trim()
 export const TENANT_ID          = process.env.REACT_APP_TENANT_ID.trim()
 export const BASE_URL           = process.env.REACT_APP_BASE_URL.trim()
-export const VERSION            = process.env.REACT_APP_VERSION.trim()
-export const INGREDIENT_PATH    = process.env.REACT_APP_INGREDIENT_PATH.trim()
-export const RECIPE_PATH        = process.env.REACT_APP_RECIPE_PATH.trim()
-export const SUPPLIER_PATH      = process.env.REACT_APP_SUPPLIER_PATH.trim()
-export const NOTIFICATION_PATH  = process.env.REACT_APP_NOTIFICATION_PATH.trim()
 export const ENABLE_GATEWAY     = process.env.REACT_APP_ENABLE_GATEWAY.trim()
+
+export const INGREDIENT_PATH    = process.env.REACT_APP_INGREDIENT_PATH.trim()
+export const INGREDIENT_URL     = process.env.REACT_APP_INGREDIENT_URL.trim()
+export const RECIPE_PATH        = process.env.REACT_APP_RECIPE_PATH.trim()
+export const RECIPE_URL         = process.env.REACT_APP_RECIPE_URL.trim()
+export const SUPPLIER_PATH      = process.env.REACT_APP_SUPPLIER_PATH.trim()
+export const SUPPLIER_URL       = process.env.REACT_APP_SUPPLIER_URL.trim()
+export const NOTIFICATION_PATH  = process.env.REACT_APP_NOTIFICATION_PATH.trim()
+export const NOTIFICATION_URL   = process.env.REACT_APP_NOTIFICATION_URL.trim()
+
 export const API_KEY            = process.env.REACT_APP_API_KEY.trim()
 export const API_PRINCIPAL      = process.env.REACT_APP_API_PRINCIPAL.trim()
 export const API_KEY_HEADER             = "x-api-key"
@@ -20,7 +26,10 @@ export const API_PRINCIPAL_HEADER       = "x-principal"
  * @returns Ingredient service url (domain/path-to-service/version)
  */
 export function baseIngredientAPI () {
-    return BASE_URL + "/" + INGREDIENT_PATH + "/" + VERSION
+    if(ENABLE_GATEWAY && ENABLE_GATEWAY === 'true'){
+        return `${BASE_URL}/${INGREDIENT_PATH}/${VERSION}`
+    }
+    return `${INGREDIENT_URL}/${INGREDIENT_PATH}/${VERSION}`
 } 
 
 /**
@@ -28,7 +37,10 @@ export function baseIngredientAPI () {
  * @returns Recipe service url (domain/path-to-service/version)
  */
 export function baseRecipeAPI () {
-    return BASE_URL + "/" + RECIPE_PATH + "/" + VERSION
+    if(ENABLE_GATEWAY && ENABLE_GATEWAY === 'true'){
+        return `${BASE_URL}/${RECIPE_PATH}/${VERSION}`
+    }
+    return `${RECIPE_URL}/${RECIPE_PATH}/${VERSION}`
 } 
 
 /**
@@ -36,7 +48,10 @@ export function baseRecipeAPI () {
  * @returns Supplier service url (domain/path-to-service/version)
  */
 export function baseSupplierAPI() {
-    return BASE_URL + "/" + SUPPLIER_PATH + "/" + VERSION
+    if(ENABLE_GATEWAY && ENABLE_GATEWAY === 'true'){
+        return `${BASE_URL}/${SUPPLIER_PATH}/${VERSION}`
+    }
+    return `${SUPPLIER_URL}/${SUPPLIER_PATH}/${VERSION}`
 }
 
 /**
@@ -44,5 +59,8 @@ export function baseSupplierAPI() {
  * @returns Notification service url (domain/path-to-service/version)
  */
 export function baseNotificationAPI() {
-    return BASE_URL + "/" + NOTIFICATION_PATH + "/" + VERSION
+    if(ENABLE_GATEWAY && ENABLE_GATEWAY === 'true'){
+        return `${BASE_URL}/${NOTIFICATION_PATH}/${VERSION}`
+    }
+    return `${NOTIFICATION_URL}/${NOTIFICATION_PATH}/${VERSION}`
 }
