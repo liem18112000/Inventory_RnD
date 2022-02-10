b2eb5846e292bfc9c0a4588db6c95d8f7c320d6a
Đoàn Văn Thanh Liêm <liem18112000@gmail.com>
2022-02-10 23:45:19 +0700

    Add template for low stock

---
+++ notification-service/src/main/java/com/fromlabs/inventory/notificationservice/notification/messages/models/LowStockDetails.java
@@ -0,0 +1,15 @@
+package com.fromlabs.inventory.notificationservice.notification.messages.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LowStockTableRow implements Serializable {
+
+
+}
