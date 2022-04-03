58502912ef6d478f01e7973ba774650b9be626d3
LongTran <hlongtd@gmail.com>
2022-04-03 18:59:50 +0700

    Updated login page

--- inventory-frontend/src/login/Login.js
+++ inventory-frontend/src/login/Login.js
@@ -2,11 +2,13 @@ import React, { useRef } from 'react';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { Password } from 'primereact/password';
-import { Checkbox } from 'primereact/checkbox';
+// import { Checkbox } from 'primereact/checkbox';
 import { Toast } from 'primereact/toast';
 import '../assets/styles/FormDemo.css';
-
-export const Login = ({onAuthenticate}) => {
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import '../assets/styles/Login.css';
+export const Login = ({ onAuthenticate }) => {
     const toast = useRef(null);
 
     const handleSubmit = e => {
@@ -23,27 +25,45 @@ export const Login = ({onAuthenticate}) => {
     };
 
     return (
-        <div className="form-demo">
+        <div className="maincontainer">
             <Toast ref={toast} />
-            <div className="p-d-flex p-jc-center">
-                <div className="card">
-                    <h3 className="p-text-center title">Inventory System</h3>
-                    <h4 className="p-text-center">Welcome back</h4>
-                    <form className="p-fluid" onSubmit={handleSubmit}>
-                        <div className="p-field">
-                            <span className="p-float-label">
-                                <InputText id="username" name="username" />
-                                <label htmlFor="username">Username</label>
-                            </span>
+            <div class="container-fluid">
+                <div class="row no-gutter">
+
+                    <div class="col-md-6 bg-light">
+                        <div class="login d-flex align-items-center py-5">
+
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-10 col-xl-7 mx-auto">
+                                        <img class="logo-image" src="https://res.cloudinary.com/ieltstinder/image/upload/v1648977464/Group_95_w4o4cb.png"></img>
+                                        <h4 class="display-5">Login</h4>
+                                        <form className="p-fluid" onSubmit={handleSubmit}>
+                                            <div className="p-field">
+                                                {/* <label htmlFor="email" className="block text-900 font-medium mb-2">Username</label> */}
+                                                <h6>Username</h6>
+                                                <span >
+                                                    <InputText placeholder="Username" id="username" name="username" />
+                                                </span>
+                                            </div>
+                                            <div className="p-field">
+                                                {/* <label htmlFor="email" className="block text-900 font-medium mb-2">Password</label> */}
+                                                <h6>Password</h6>
+                                                <span>
+                                                    <Password placeholder="Password" id="password" name="password" toggleMask feedback={false} />
+                                                </span>
+                                            </div>
+
+                                            <label><input type="checkbox" /> Remember Me</label>
+                                            <Button type="submit" label="Login" className="p-mt-2" />
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="p-field">
-                            <span className="p-float-label">
-                                <Password id="password" name="password" toggleMask feedback={false} />
-                                <label htmlFor="password" >Password</label>
-                            </span>
-                        </div>
-                        <Button type="submit" label="Submit" className="p-mt-0" />
-                    </form>
+                    </div>
+                    <div class="col-md-6 d-none d-md-flex bg-image"></div>
+
                 </div>
             </div>
         </div>
